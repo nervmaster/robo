a88959035b638fa93dd3f02cd16541af2c977515
Henrique Pereira Borges <hpborges@gmail.com>
2017-07-24 09:36:56 -0300

    A* feito e operante.
    Falta desenhar um caminho para mandar para o robo

--- src/Robot.java
+++ src/Robot.java
@@ -1,7 +1,11 @@
-public class Robot implements Navigator {
-    //Construtor
-    public Robot() {
+import java.util.Arrays;
 
+public class Robot implements Navigator {
+    private int[][] obs;
+
+
+    public Robot() {
+        obs = new int[][]{{3,4},{4,2},{3,3},{3,5},{1,4},{0,5}};
     }
     //Metodos da interface
     public void goTo(int[] dir) {
@@ -9,9 +13,37 @@ public class Robot implements Navigator {
     }
 
     public boolean[] getObstacle() {
-        return new boolean[]{true,false,false};
+        return new boolean[]{false,true,false};
     }
 
+    public boolean[] fooGetObstacle(int[] pos) {
+        int x,y;
+        boolean[] result = new boolean[]{false,false,false};
+        for(int[] obs : this.obs) {
+            //abaixo
+            x = pos[0];
+            y = pos[1]-1;
+            if(Arrays.equals(new int[]{x,y}, obs)) {
+                result[0] = true;
+            }
+            
+            //esquerda
+            x = pos[0]-1;
+            y = pos[1];
+            if(Arrays.equals(new int[]{x,y}, obs)) {
+                result[1] = true;
+            }
+
+            //acima
+            x = pos[0];
+            y = pos[1]+1;
+            if(Arrays.equals(new int[]{x,y}, obs)) {
+                result[2] = true;
+            }
+        }
+        return result;
+    }
+        
     public int[] getPosition() {
         return new int[]{4,4,0,4};
     } 
--- src/Star.java
+++ src/Star.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.lang.Math;
 
 public class Star{
@@ -10,6 +11,7 @@ public class Star{
     //Acesso de matriz é em mat[lin][col] ou mat[y][x]
     int[][] desl;
     int[][] est;
+    
 
     Robot robot;
     
@@ -23,8 +25,58 @@ public class Star{
         return custo_x + custo_y;
     }
 
-    private int getMatValue(int[][] mat, int x, int y) {
-        return mat[y][x];
+    private boolean isNeighbor(int x, int y){
+        return (Math.abs(x - pos[0]) <= 1 || Math.abs(y = pos[1]) <= 1);
+    }
+
+    private void updateCost(boolean[] obs, int[] pos) {
+            int x = pos[0];
+            int y = pos[1];
+
+            //custo deslocamento ate a celula atual
+            int custo_atual = desl[y][x];
+
+            //Custo de deslocamento para celulas vizinhas se não tem obstaculo
+            if(y > 0 && desl[y-1][x] >= 0) {
+                desl[y-1][x] = obs[0] ? -1 : custo_atual + 1; //abaixo
+                est[y-1][x] = obs[0] ? -1 : estimateCost(x, y-1);
+            }
+            if(x > 0 && desl[y][x-1] >= 0) {
+                desl[y][x-1] = obs[1] ? -1 : custo_atual + 1; //esquerda
+                est[y][x-1] = obs[1] ? -1 : estimateCost(x-1, y);
+            }
+            if(y < desl[0].length && desl[y+1][x] >= 0) {
+                desl[y+1][x] = obs[2] ? -1 : custo_atual + 1; //acima
+                est[y+1][x] = obs[2] ? -1 : estimateCost(x, y+1);
+            }
+
+            //Nulifica os valores da celula atual
+            est[y][x] = -10;
+            desl[y][x] = -10;
+
+    }
+
+    private int[] lowestCellValue() {
+        int lowest = -1;
+        int[] result = {-1,-1};
+        for(int x = 0; x < desl.length; x++) {
+            for(int y = 0; y < desl[0].length; y++) {
+                int value = desl[y][x] + est[y][x];
+                if(value > 0 && (lowest < 0 || value < lowest)) {
+                    System.out.printf("old: %d new %d pos %d,%d\n", lowest, value, x, y);
+                    lowest = value;
+                    result = new int[]{x,y};
+                }
+
+                //Prioridade para uma celula vizinha conseguir o valor
+                // else if(value > 0 && (value <= lowest && isNeighbor(x,y))) {
+                //     System.out.printf("old: %d new %d pos %d,%d\n", lowest, value, x, y);
+                //     lowest = value;
+                //     result = new int[]{x,y};
+                // }
+            }
+        }
+        return result;
     }
 
     //Retorna uma matriz NxM
@@ -53,7 +105,7 @@ public class Star{
 
     public void print() {
         //Inverte os valores y para que o 0,0 fique no canto inferior esquerdo ao invés do canto superior
-        for(int y = desl.length-1; y > 0; y--) {
+        for(int y = desl.length-1; y >= 0; y--) {
             for(int x = 0; x < desl[0].length; x++) {
                 int[] cell = {x,y};
                 if(Arrays.equals(this.pos, cell)) {
@@ -65,6 +117,9 @@ public class Star{
                 else if(desl[y][x] == -1){
                     System.out.printf("X ");
                 }
+                else if(desl[y][x] == -10){
+                    System.out.printf("- ");
+                }
                 else {
                     System.out.printf("%d ", desl[y][x] + est[y][x]);
                 }
@@ -74,33 +129,32 @@ public class Star{
         System.out.print("\n");
     }
     public void start() {
-        //Calcula pesos
-        int x = this.pos[0];
-        int y = this.pos[1];
+        Scanner keyboard = new Scanner(System.in);
 
-        //Verifica se tem obstaculos vizinhos
-        boolean[] obs = robot.getObstacle();
-        
-        //custo deslocamento ate a celula atual
-        int custo_atual = getMatValue(desl, x, y);
+        while(!Arrays.equals(this.pos, this.chegada)){
 
-        //Custo de deslocamento para celulas vizinhas se não tem obstaculo
-        desl[y-1][x] = obs[0] ? -1 : custo_atual + 1; //abaixo
-        desl[y][x-1] = obs[1] ? -1 : custo_atual + 1; //esquerda
-        desl[y+1][x] = obs[2] ? -1 : custo_atual + 1; //acima
+            //Calcula pesos
+            int x = this.pos[0];
+            int y = this.pos[1];
 
-        //Estima o custo de deslocamento das celulas vizinhas ate a chegada
-        est[y-1][x] = obs[0] ? -1 : estimateCost(x, y-1);
-        est[y][x-1] = obs[1] ? -1 : estimateCost(x-1, y);
-        est[y+1][x] = obs[2] ? -1 : estimateCost(x, y+1);
+            //Verifica se tem obstaculos vizinhos
+            //boolean[] obs = robot.getObstacle();
+            boolean[] obs = robot.fooGetObstacle(this.pos);
 
-        //Vai ate a celula de menor valor
-        //Procura celular de menor valor
-        //Calcula caminho JÁ visitado para a celula de menor valor
-        //Manda para o robo o caminho
+            updateCost(obs, this.pos);
+            
+            this.print();
+            keyboard.next();
 
-        //Repeat
+            //Vai ate a celula de menor valor
+            int[] target_pos = lowestCellValue();
 
+            //Calcula caminho JÁ visitado para a celula de menor valor
+            //Manda para o robo o caminho
+            //SIMULAÇÃO
+            this.pos = target_pos;
+            System.out.printf("%d,%d\n",this.pos[0], this.pos[1]);
+        }
     }
 
     //MAIN
@@ -114,7 +168,6 @@ public class Star{
 
         //Cria instância A*
         Star star = new Star(r, saida, chegada);
-        star.print();
         star.start();
         star.print();
 
